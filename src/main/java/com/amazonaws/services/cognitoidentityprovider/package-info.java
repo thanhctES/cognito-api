77ad35779b2f934956878736b6d159b23e9736d9
Francisco Garrido Mosqueira <garridof@ryanair.com>
2018-01-23 15:40:31 +0100

    Inital commit

---
+++ src/main/java/com/amazonaws/services/cognitoidentityprovider/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <p>Using the Amazon Cognito Your User Pools API, you can create a user pool to manage directories and users. You can authenticate a user to obtain tokens related to user identity and access policies.</p> <p>This API reference provides information about user pools in Amazon Cognito Your User Pools.</p> <p>For more information, see the Amazon Cognito Documentation.</p>
+ */
+
+package com.amazonaws.services.cognitoidentityprovider;
+
